676497e5a05a45d358a6fb397c4aac0ceb2c7370
menezes <arthurmenezespinho2008@gmail.com>
2025-01-22 09:04:59 -0300

    Ajustado validações

--- gerador_de_senha/Program.cs
+++ gerador_de_senha/Program.cs
@@ -11,18 +11,20 @@ while(!(int.TryParse(Console.ReadLine(), out int resultado))){
 Console.WriteLine("Informe se deseja números nas senha (S ou N): ");
 do{
     senha_numero_vef = Console.ReadLine();
-    if(senha_numero_vef != "s" && senha_numero_vef != "s" && senha_numero_vef != "N" && senha_numero_vef != "n"  )
+    if(senha_numero_vef != "s" && senha_numero_vef != "n"  )
         Console.WriteLine("Informe um reposta válida! (somente S ou N)");
-}while(senha_numero_vef != "s" && senha_numero_vef != "s" && senha_numero_vef != "N" && senha_numero_vef != "n"  );
+}while(senha_numero_vef != "s" && senha_numero_vef != "n"  );
+
 Console.WriteLine("Informe se deseja letras nas senha (S ou N): ");
 do{
     senha_letras_vef = Console.ReadLine();
-    if(senha_letras_vef != "s" && senha_letras_vef != "s" && senha_letras_vef != "N" && senha_letras_vef != "n"  )
+    if(senha_letras_vef != "s" && senha_letras_vef != "n"  )
         Console.WriteLine("Informe um reposta válida! (somente S ou N)");
-}while(senha_letras_vef != "s" && senha_letras_vef != "s" && senha_letras_vef != "N" && senha_letras_vef != "n"  );
+}while(senha_letras_vef != "s" && senha_letras_vef != "n"  );
+
 Console.WriteLine("Informe se deseja caracteres especiais nas senha (S ou N): ");
 do{
     senha_especiais_vef = Console.ReadLine();
-    if(senha_especiais_vef != "s" && senha_especiais_vef != "s" && senha_especiais_vef != "N" && senha_especiais_vef != "n"  )
+    if(senha_especiais_vef != "s" && senha_especiais_vef != "n"  )
         Console.WriteLine("Informe um reposta válida! (somente S ou N)");
-}while(senha_especiais_vef != "s" && senha_especiais_vef != "s" && senha_especiais_vef != "N" && senha_especiais_vef != "n"  );
+}while(senha_especiais_vef != "s" && senha_especiais_vef != "n"  );
