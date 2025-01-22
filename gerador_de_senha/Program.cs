ea346cd60d9f9ab0af9da0a0137d9e0532e7f962
menezes <arthurmenezespinho2008@gmail.com>
2025-01-22 09:06:05 -0300

    alterado valores coletados para mini

--- gerador_de_senha/Program.cs
+++ gerador_de_senha/Program.cs
@@ -10,21 +10,21 @@ while(!(int.TryParse(Console.ReadLine(), out int resultado))){
 }
 Console.WriteLine("Informe se deseja números nas senha (S ou N): ");
 do{
-    senha_numero_vef = Console.ReadLine();
+    senha_numero_vef = Console.ReadLine().ToLower();
     if(senha_numero_vef != "s" && senha_numero_vef != "n"  )
         Console.WriteLine("Informe um reposta válida! (somente S ou N)");
 }while(senha_numero_vef != "s" && senha_numero_vef != "n"  );
 
 Console.WriteLine("Informe se deseja letras nas senha (S ou N): ");
 do{
-    senha_letras_vef = Console.ReadLine();
+    senha_letras_vef = Console.ReadLine().ToLower();
     if(senha_letras_vef != "s" && senha_letras_vef != "n"  )
         Console.WriteLine("Informe um reposta válida! (somente S ou N)");
 }while(senha_letras_vef != "s" && senha_letras_vef != "n"  );
 
 Console.WriteLine("Informe se deseja caracteres especiais nas senha (S ou N): ");
 do{
-    senha_especiais_vef = Console.ReadLine();
+    senha_especiais_vef = Console.ReadLine().ToLower();
     if(senha_especiais_vef != "s" && senha_especiais_vef != "n"  )
         Console.WriteLine("Informe um reposta válida! (somente S ou N)");
 }while(senha_especiais_vef != "s" && senha_especiais_vef != "n"  );
