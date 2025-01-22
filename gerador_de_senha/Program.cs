3fe4cecc4286354add234df56bd204c1a8970b6f
menezes <arthurmenezespinho2008@gmail.com>
2025-01-22 08:55:44 -0300

    Criado variavel para verificar se o user deseja numeros e validando dados

--- gerador_de_senha/Program.cs
+++ gerador_de_senha/Program.cs
@@ -1,6 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+string senha_numero_vef;
 Console.WriteLine("Gerador de senhas!");
 Console.WriteLine("Insira o tamanho da senha: ");
 while(!(int.TryParse(Console.ReadLine(), out int resultado))){
     Console.WriteLine("Insira um valor válido! Somente números inteiros!");
 }
+Console.WriteLine("Informe seja deseja números nas senha (S ou N): ");
+do{
+    senha_numero_vef = Console.ReadLine();
+    if(senha_numero_vef != "s" && senha_numero_vef != "s" && senha_numero_vef != "N" && senha_numero_vef != "n"  )
+        Console.WriteLine("Informe um reposta válida! (somente S ou N)");
+}while(senha_numero_vef != "s" && senha_numero_vef != "s" && senha_numero_vef != "N" && senha_numero_vef != "n"  );
