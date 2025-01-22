83c4aeb37c041b8b591e9af48ca0e7155bdf8655
menezes <arthurmenezespinho2008@gmail.com>
2025-01-22 08:40:38 -0300

    Criado variavel resultado e validando os dados dela

--- gerador_de_senha/Program.cs
+++ gerador_de_senha/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Gerador de senhas!");
-
+Console.WriteLine("Insira o tamanho da senha: ");
+while(!(int.TryParse(Console.ReadLine(), out int resultado))){
+    Console.WriteLine("Insira um valor válido! Somente números inteiros!");
+}
