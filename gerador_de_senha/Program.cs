9ca04cc778930d0e9ab17c973533a713314cb487
menezes <arthurmenezespinho2008@gmail.com>
2025-01-22 08:58:15 -0300

    Criado estrutura para coletar e validar valores

--- gerador_de_senha/Program.cs
+++ gerador_de_senha/Program.cs
@@ -6,9 +6,15 @@ Console.WriteLine("Insira o tamanho da senha: ");
 while(!(int.TryParse(Console.ReadLine(), out int resultado))){
     Console.WriteLine("Insira um valor válido! Somente números inteiros!");
 }
-Console.WriteLine("Informe seja deseja números nas senha (S ou N): ");
+Console.WriteLine("Informe se deseja números nas senha (S ou N): ");
 do{
     senha_numero_vef = Console.ReadLine();
+    if(senha_numero_vef != "s" && senha_numero_vef != "s" && senha_numero_vef != "N" && senha_numero_vef != "n"  )
+        Console.WriteLine("Informe um reposta válida! (somente S ou N)");
+}while(senha_numero_vef != "s" && senha_numero_vef != "s" && senha_numero_vef != "N" && senha_numero_vef != "n"  );
+Console.WriteLine("Informe se deseja letras nas senha (S ou N): ");
+do{
+    senha_letras_vef = Console.ReadLine();
     if(senha_numero_vef != "s" && senha_numero_vef != "s" && senha_numero_vef != "N" && senha_numero_vef != "n"  )
         Console.WriteLine("Informe um reposta válida! (somente S ou N)");
 }while(senha_numero_vef != "s" && senha_numero_vef != "s" && senha_numero_vef != "N" && senha_numero_vef != "n"  );
