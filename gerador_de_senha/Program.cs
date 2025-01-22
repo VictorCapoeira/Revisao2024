91917927835cfb4ebd2b304cc6d524094ceed09c
menezes <arthurmenezespinho2008@gmail.com>
2025-01-22 09:09:25 -0300

    Adicionado função clear

--- gerador_de_senha/Program.cs
+++ gerador_de_senha/Program.cs
@@ -8,20 +8,21 @@ Console.WriteLine("Insira o tamanho da senha: ");
 while(!(int.TryParse(Console.ReadLine(), out int resultado))){
     Console.WriteLine("Insira um valor válido! Somente números inteiros!");
 }
+Console.Clear();
 Console.WriteLine("Informe se deseja números nas senha (S ou N): ");
 do{
     senha_numero_vef = Console.ReadLine().ToLower();
     if(senha_numero_vef != "s" && senha_numero_vef != "n"  )
         Console.WriteLine("Informe um reposta válida! (somente S ou N)");
 }while(senha_numero_vef != "s" && senha_numero_vef != "n"  );
-
+Console.Clear();
 Console.WriteLine("Informe se deseja letras nas senha (S ou N): ");
 do{
     senha_letras_vef = Console.ReadLine().ToLower();
     if(senha_letras_vef != "s" && senha_letras_vef != "n"  )
         Console.WriteLine("Informe um reposta válida! (somente S ou N)");
 }while(senha_letras_vef != "s" && senha_letras_vef != "n"  );
-
+Console.Clear();
 Console.WriteLine("Informe se deseja caracteres especiais nas senha (S ou N): ");
 do{
     senha_especiais_vef = Console.ReadLine().ToLower();
